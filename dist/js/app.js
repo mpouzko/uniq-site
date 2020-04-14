cbcb2a53dc170c36fd94fd3ae3172b3d9e8a6eae
mpouzko <pouzko@gmail.com>
2020-04-14 11:45:32 +0300

    cleanup

--- dist/js/app.js
+++
@@ -1,2 +0,0 @@
-"use strict";window.addEventListener("DOMContentLoaded",function(){});var firstmod=function(t){alert("first module");return 5-Date.now()+t},second_mod=function(){alert("secod mod")};throw"alert";
-//# sourceMappingURL=app.js.map
